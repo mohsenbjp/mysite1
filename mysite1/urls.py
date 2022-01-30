c605606b6707ac093515b883ac824b85ea9603d2
Mohsen <mohsenbjp@gmail.com>
2022-01-30 09:01:51 +0330

    11.11.1400

--- mysite1/urls.py
+++ mysite1/urls.py
@@ -1,7 +1,7 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path,include, re_path
 from .views import *
 from django.contrib.sitemaps.views import sitemap
 from .sitemaps import StaticViewSitemap
@@ -22,6 +22,7 @@ urlpatterns = [
     path('sitemap.xml',sitemap,{'sitemaps':sitemaps},name='django.contrib.sitemaps.views.sitemap'),
     path('robots.txt',include('robots.urls')),
     path('accounts/', include('allauth.urls')),
+    re_path(r"^.*",index),
 
 ]
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
